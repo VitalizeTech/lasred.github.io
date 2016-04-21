cbe1e353dafb22cf7effc150154d40c349605755
kalexander12 <katielynnalexander@hotmail.com>
2016-04-21 12:53:57 -0700

    Add scoring details and moved score board to right of map

--- main.js
+++ main.js
@@ -161,6 +161,20 @@ TrumpWalker.prototype.update = function () {
                 // collision detected! 
                 this.game.activeVoteCoins.splice(i, 1);
                 this.game.scoreBoard.innerHTML = voteCoin.vote + parseInt(this.game.scoreBoard.innerHTML);
+                if(this.game.scoreType.innerHTML == "Delegates") {
+					if(this.game.scoreBoard.innerHTML >= 50) {
+						this.game.scoreMessage.innerHTML = this.game.scoreMessage.innerHTML.replace('  ', 'You won the Republican nomination!');
+						this.game.scoreType.innerHTML = this.game.scoreType.innerHTML.replace('Delegates', 'Electoral Votes');
+						this.game.scoreBoard.innerHTML = 0;
+					}
+				}	
+				else if(this.game.scoreType.innerHTML == "Electoral Votes") {
+					if(this.game.scoreBoard.innerHTML >= 50) {
+						this.game.scoreMessage.innerHTML = this.game.scoreMessage.innerHTML.replace('You won the Republican nomination!', 'You won the presidential election!');
+						gameEngine.pause();
+						//pop up new screen for congrats or enter name for high score or something?
+					}
+				}
                 var pendingLength = this.game.pendingVoteCoins.length;
                 if (pendingLength > 0) {
                     var toAddVoteCoin = this.game.pendingVoteCoins[pendingLength - 1];
@@ -222,10 +236,14 @@ AM.queueDownload("./img/MAGABackground.png");
 AM.downloadAll(function () {
     var canvas = document.getElementById("gameWorld");
     var score = document.getElementById("score")
+    var message = document.getElementById("message");
+	var voteType = document.getElementById("score_type");
     //2d context 
     var ctx = canvas.getContext("2d");
     var gameEngine = new GameEngine();
     gameEngine.scoreBoard = score;
+    gameEngine.scoreMessage = message;
+	gameEngine.scoreType = voteType;
     gameEngine.activeVoteCoins = [new VoteCoin(60, 55, "New York", 95), new VoteCoin(700, 55, "Connecticut", 28),
 	   new VoteCoin(60, 450, "Delaware", 16), new VoteCoin(700, 450, "Rhode Island", 19)];
     gameEngine.pendingVoteCoins = [new VoteCoin(180, 55, "Maryland", 38), new VoteCoin(444, 80, "Pennsylvania", 17),
