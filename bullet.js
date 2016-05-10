f8e56c4628f146d66ad434fae2b68f966457aac8
arshdeep <arshsingh_sc@yahoo.com>
2016-05-10 11:15:57 -0700

    the bullet now shoots off in a random direction when it spawns

--- bullet.js
+++ bullet.js
@@ -1,16 +1,39 @@
+
+
+
+
+var potentialX = [-4, -3.5, -3, -2.5, -2, -1.5, -1, -.5, 0, .5, 1, 1.5, 2, 2.5, 3, 3.5, 4];
+var potentialY = [0, .5, 1, 1.5, 2, 2.5, 3, 3.5, 4, -3.5, -3, -2.5, -2, -1.5, -1, -.5, 0];
+
+
+
 function Bullet(game, spritesheet) {
 	this.spriteSheet = spritesheet;
     this.game = game;
     this.ctx = game.ctx;
-    this.x = 300;
+    this.x = 350;
     this.speed = 10;
-    this.y = 220;
+    this.y = 250;
+
+	//get the direction for this bullet.
+	this.moves = Math.floor(Math.random() * potentialX.length);
+
 }
 
 Bullet.prototype.update = function () {
-    this.x -= 0.8;
-	if(this.x < 0) {
-		this.x = 300;
+
+    this.x += potentialX[this.moves];
+	this.y += potentialY[this.moves];
+
+	if(this.x < 0 || this.x > 650) {
+		this.moves = Math.floor(Math.random() * potentialX.length);
+		this.x = 350;
+		this.y = 250;
+	}
+	if (this.y < 0 || this.y > 450) {
+		this.moves = Math.floor(Math.random() * potentialX.length);
+		this.y = 250;
+		this.x = 350;
 	}
 }
 
@@ -18,4 +41,3 @@ Bullet.prototype.draw = function (ctx) {
     this.ctx.drawImage(this.spriteSheet,
                  this.x, this.y, 30, 30);
 };
-
