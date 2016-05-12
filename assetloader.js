8d19e8b93255baf3c33afccb57538402ecc7af87
Chris Sun <chris438@gmail.com>
2016-05-12 13:59:47 -0700

    Adding other characters to game. Other character should follow Donald

--- assetloader.js
+++ assetloader.js
@@ -1,14 +1,15 @@
 var AM = new AssetManager();
 
 AM.queueDownload("./img/Trump.png");
-AM.queueDownload("./img/gunman.png");
-AM.queueDownload("./img/bluehairedgirl.png");
 AM.queueDownload("./img/MAGABackground.png");
 AM.queueDownload("./img/flags/spritesheet.png");
 AM.queueDownload("./img/CrookedHillary.png");
 AM.queueDownload("./img/LyinTed.png");
 AM.queueDownload("./img/Canada.png");
-
+AM.queueDownload("./img/ivanka.png");
+AM.queueDownload("./img/reporter.png");
+AM.queueDownload("./img/cartel.png");
+AM.queueDownload("./img/assassin.png");
 AM.downloadAll(function () {
     var canvas = document.getElementById("gameWorld");
     canvas.focus();
@@ -27,9 +28,14 @@ AM.downloadAll(function () {
     gameEngine.init(ctx);
     gameEngine.start();
     gameEngine.addEntity(new Background(gameEngine));
-    gameEngine.addEntity(new TrumpWalker(gameEngine, AM.getAsset("./img/Trump.png")));
-    gameEngine.addEntity(new Gunman(gameEngine, AM.getAsset("./img/gunman.png")));
-    gameEngine.addEntity(new BlueHair(gameEngine, AM.getAsset("./img/bluehairedgirl.png")));
+	var trumpWalker = new TrumpWalker(gameEngine, AM.getAsset("./img/Trump.png"));
+	gameEngine.leader = trumpWalker;
+    gameEngine.addEntity(trumpWalker);
+    gameEngine.addEntity(new IvankaWalker(gameEngine, AM.getAsset("./img/ivanka.png"), 32, 50, 3, 200, 250, 0.1, 4, 100));
 	gameEngine.addEntity(new Bullet(gameEngine, AM.getAsset("./img/Canada.png")))
+	gameEngine.addEntity(new ReporterWalker(gameEngine, AM.getAsset("./img/reporter.png"), 175, 150, 11, 500, 100, 0.5, 100, 70));
+	gameEngine.addEntity(new CartelWalker(gameEngine, AM.getAsset("./img/cartel.png"), 75, 190, 4, 500, 400, 0.5, 100, 70));
+	gameEngine.addEntity(new AssassinWalker(gameEngine, AM.getAsset("./img/assassin.png"), 33, 48, 4, 100,  400, 0.1, 4, 100));
+
 })
 var FLAGS = AM.getAsset("./img/flags/spritesheet.png");
