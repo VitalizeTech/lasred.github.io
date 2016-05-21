ee66bf364aa561bbb0a7cd98d00d9e6e5446caf0
kalexander12 <katielynnalexander@hotmail.com>
2016-05-20 22:36:41 -0700

    Update bullet.js

--- bullet.js
+++ bullet.js
@@ -18,9 +18,9 @@ Bullet.prototype.update = function () {
 	this.x += Math.cos( toRadians(this.degree + 180)) * this.speed;
 	this.y += Math.sin( toRadians(this.degree + 180)) * this.speed;
 	if(this.x < 0 || this.x > 1060  || this.y < 0 || this.y > 700) {
-        	if(this.game.entities.length > 9) {
-            		this.game.entities.splice(6, 1);
-        	}
+        if(this.game.entities.length > 9) {
+            this.game.entities.splice(6, 1);
+        }
 		//this.game.removeEntity(this.position);
 	} 
 }
