096d40232efcf131d98af5b2eb8576e2af4505ac
kalexander12 <katielynnalexander@hotmail.com>
2016-05-22 18:01:33 -0700

    Update ReporterWalker.js
    fixed reporter boost so that it doesn't last as long

--- ReporterWalker.js
+++ ReporterWalker.js
@@ -75,17 +75,13 @@ ReporterWalker.prototype.update = function () {
         if(Math.random() > .5) {
             this.game.scoreMessage.innerHTML = 'The Reporter caught up to you for an interview and you have recieved a boost to your popularity for a short time. This will increase your popularity in each state.';
             this.game.reporterBoost = 1.5;
-            this.reportTimer = 2000;
+            this.reportTimer = 1000;
         }
         else {
             this.game.scoreMessage.innerHTML = 'The Reporter caught up to you for an interview and you have recieved a blow to  your popularity for a short time. This will reduce your popularity in each state.';
             this.game.reporterBoost = .75;
-            this.reportTimer = 2000;
+            this.reportTimer = 1000;
         }
-        if(this.reportTimer < 1) {
-            this.game.reporterBoost = 1;
-        }
-        
         if(Math.random() > .5) {
             this.game.entities[3].x = ((Math.random() * 2170) + 1400);
             if(Math.random() > .5) {
@@ -105,6 +101,9 @@ ReporterWalker.prototype.update = function () {
             }
         }
         
+    }
+    if(this.reportTimer < 1) {
+            this.game.reporterBoost = 1;
     }
     //calculate next directional move
     if(Math.random() > .95) {
